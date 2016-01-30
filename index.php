c8e30e257f16ddfad71dd935ffd5b8e3d0125469
mattias0h <hintmattias@gmail.com>
2016-01-30 14:41:03 +0200

    Lecture 2 - Ajax with PHP using jQuery - creating the environment [#111841533]

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ajax Tutorials</title>
+    <script src="jquery-2.2.0.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+
+    <style>
+        body {
+            padding: 0;
+            margin: 0;
+            font-family: Arial;
+        }
+
+        #box {
+            background-color: gray;
+            text-align: center;
+            padding: 10px;
+            color: white;
+            width: 500px;
+            height: 300px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+
+
+<div id="box">
+    <h2>New user register here:</h2>
+    <input type="text" name="name" id="name" placeholder="Enter Your Name">
+    <br>
+    <input type="button" name="sub" value="Submit" id="sub">
+    <div id="result"></div>
+</div>
+
+
+</body>
+</html>
